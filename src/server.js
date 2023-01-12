db20000e4d4fed41f851e4fe66c39af6796e2d96
Hai Long <hailongkc1203@gmail.com>
2023-01-12 15:27:18 +0700

    Template (View) Engine

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const path = require('path');
 const app = express()
-const port = 8080
+const port = 8080;
+
+//congif template engine
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
 app.get('/abc', function (req, res) {
-    res.send('Check ABC')
+    // res.send('Check ABC')
+    res.render('sample')
 })
 
 app.listen(port, () => {
