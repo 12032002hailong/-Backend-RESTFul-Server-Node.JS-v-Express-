e896a52838396791758e71a1758a71da31650219
Hai Long <hailongkc1203@gmail.com>
2023-01-12 15:13:59 +0700

    routes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 8080
 
-const hostname = 'localhost';
-const port = 8080;
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+app.get('/abc', function (req, res) {
+    res.send('Check ABC')
+})
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+    console.log(`Example app listen on port ${port}`);
 })
